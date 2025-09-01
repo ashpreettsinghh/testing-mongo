352275afd9a19256855c740c92dadc06c32b08cd
Ashpreet Singh <84430991+ashpreettsinghh@users.noreply.github.com>
2025-09-02 00:38:26 +0530

    Update textract.py

--- textract.py
+++ textract.py
@@ -1,1 +1,1 @@
-segment = [x if (x.endswith('\n') and ('<' in x and '>' in x)) else x.strip() for x in segment.split('\n') if x.strip() or x.endswith('\n')]
+segment = [x if (x.endswith('\n') and ('<' in x and '>' in x)) else x.strip() for x in segment.split('\n') if x.strip()]
